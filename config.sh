598e2e659d2eb15002400bb6cd8fd2aa758531b0
gorkemoji <emircan9921@outlook.com>
2022-08-17 23:37:21 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://github.com/gorkemoji/android_device_samsung_zerolteusc.git -b master"
+export DT_LINK="https://github.com/gorkemoji/android_device_samsung_zerolteusc.git -b fox_9.0-testing"
 
 export DEVICE="zerolteusc"
 export OEM="samsung"
